06a0296be4535bc5fb32cfcdaf6077ec2796b7ba
liamyoon <liam.yoon@nexr.com>
2021-01-27 17:18:49 +0900

    example update

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,44 @@
 import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin,
+  ILayoutRestorer,
 } from '@jupyterlab/application';
+import { ICommandPalette } from '@jupyterlab/apputils';
+import { LabIcon } from '@jupyterlab/ui-components';
+import { CustomWidget } from './CustomWidget';
+import speakerIconSvg from '../style/icon/Speaker_Icon.svg';
+
+const WIDGET_ID = 'custom-widget-extension';
+
+const speakerIcon = new LabIcon({
+  name: 'custom-ui-components:speaker',
+  svgstr: speakerIconSvg,
+});
 
 /**
  * Initialization data for the react-widget extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
-  id: 'my-custom-widget',
+  id: 'react-widget',
   autoStart: true,
-  optional: [],
+  optional: [
+    ICommandPalette,
+    ILayoutRestorer,
+  ],
   activate: (
     app: JupyterFrontEnd,
+    palette: ICommandPalette,
+    restorer: ILayoutRestorer,
   ) => {
-    const { commands } = app;
+    // 위젯 생성
+    const widget = new CustomWidget();
+    widget.id = WIDGET_ID;
+    widget.title.icon = speakerIcon;
 
-    const command = 'example:on-load';
-    commands.addCommand(command, {
-      label: 'Execute example:main-menu Command',
-      caption: 'Execute example:main-menu Command',
-      execute: () => {
-        console.log('onload labextension example 2');
-      },
-    });
-    app.commands.execute(command);
+    // 메뉴 복원 추척에 등록
+    restorer.add(widget, WIDGET_ID);
+    // left 메뉴에 위젯 추가.
+    app.shell.add(widget, 'left', { rank: 200 });
   },
 };
 
